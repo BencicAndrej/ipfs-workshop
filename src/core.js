ef4b9f9a15e684a7bf1d265351ffc8b2947baa06
Andrej Bencic <bencicandrej94@gmail.com>
2018-10-17 16:57:18 +0200

    [Finished] Relative node path. [Setup] Encrypt nodes on put.

--- src/core.js
+++ src/core.js
@@ -6,9 +6,11 @@ const crypto = require("./util/crypto");
 async function putCommand(documentPath, options = {}) {
     const documentRoot = parseDocument(documentPath);
 
-    const ipfsRoot = await createIPFSNode(documentRoot);
+    const ipfsRoot = await createIPFSNode(documentRoot, options.password);
 
-    console.log(ipfsRoot.toJSON().multihash);
+    const documentNode = parseIPFSObject(ipfsRoot);
+
+    console.log(JSON.stringify(documentNode));
 }
 
 function parseDocument(documentPath) {
@@ -17,13 +19,13 @@ function parseDocument(documentPath) {
     return JSON.parse(data);
 }
 
-async function createIPFSNode(node) {
+async function createIPFSNode(node, password = null) {
     const ipfs = await IPFS.instance();
 
     let obj = await ipfs.object.new();
 
     for (let link in node.links || {}) {
-        const linkNode = await createIPFSNode(node.links[link]);
+        const linkNode = await createIPFSNode(node.links[link], password);
 
         obj = await ipfs.object.patch.addLink(obj.multihash, {
             name: link,
@@ -33,15 +35,23 @@ async function createIPFSNode(node) {
     }
 
     let data = JSON.stringify(node.data);
+    if (password) {
+        //@TODO: Encrypt node data on put.
+    }
 
     return await ipfs.object.patch.setData(
         obj.multihash, Buffer.from(data));
 }
 
 async function getCommand(hash, options = {}) {
+    let path;
+    [hash, ...path] = hash.split("/").filter(el => el.length > 0);
+
     let documentNode = await getDocumentNodeByHash(hash);
 
-    //@TODO: Extract hash and relative path from hash and get node at that path.
+    if (path.length > 0) {
+        documentNode = await getDocumentNodeAtPath(documentNode, path);
+    }
 
     if (options.expand) {
         documentNode = await expandDocumentNodeLinks(documentNode);
@@ -59,7 +69,18 @@ async function getDocumentNodeByHash(hash) {
 }
 
 async function getDocumentNodeAtPath(node, path) {
-    //@TODO: Implement relative node paths.
+    for (let i = 0; i < path.length; i++) {
+        const linkName = path[i];
+
+        const linkHash = node.links[linkName];
+        if (!linkHash) {
+            throw new Error(`link not found: ${linkName}`)
+        }
+
+        node = await getDocumentNodeByHash(linkHash);
+    }
+
+    return node;
 }
 
 function parseIPFSObject(object) {
